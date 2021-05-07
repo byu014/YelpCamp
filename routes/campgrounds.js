4a79f1d7c57e902896e3783ab077709eff7f4fa9
byu014 <byu014@ucr.edu>
2021-05-06 18:52:18 -0700

    restructured app.js to use routers for campgrounds and reviews

---
+++ routes/campgrounds.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+
+const Campground = require('../models/campground');
+
+const {campgroundSchema} = require('../schemas.js');
+
+const validateCampground = (req, res , next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    console.log(error);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg,400)
+    }else{
+        next();
+    }
+}
+
+router.get('/', catchAsync(async (req, res) =>{
+    const campgrounds = await Campground.find();
+    res.render('campgrounds/index', {campgrounds});
+}))
+
+router.get('/new', (req,res) => {
+    res.render('campgrounds/new');
+})
+
+router.post('/', validateCampground,catchAsync(async(req, res) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+}))
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id).populate('reviews');
+    res.render('campgrounds/show', {campground}); 
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+}))
+
+router.patch('/:id', validateCampground, catchAsync(async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', {campground});
+}))
+
+module.exports = router;
