8217d2029d84703088cb21d843a5701d9d6254f5
byu014 <byu014@ucr.edu>
2021-06-04 17:32:13 -0700

    extremely minor syntax fixes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ mongoose.connect(dbUrl ,{
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connectione rror:"));
+db.on('error', console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log('DB connected');
 });
